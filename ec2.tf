67b92d29b7926a2bec9830c9b36a5c3cbd22d713
vinod sjml <vinsjml@gmail.com>
2024-02-16 19:45:52 +0530

    push

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # create spot instance
 resource "aws_spot_instance_request" "spot" {
   count                = var.SPOT_INSTANCE_COUNT
-  ami                  = data.aws_ami.ami.name
+  ami                  = data.aws_ami.ami.image_id
   instance_type        = var.SPOT_INSTANCE_TYPE 
   wait_for_fulfillment = true
   subnet_id            = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
@@ -11,7 +11,7 @@ resource "aws_spot_instance_request" "spot" {
 # created OD instamces
 resource "aws_instance" "od" {
  count                 = var.OD_INSTANCE_COUNT
-  ami                  = data.aws_ami.ami.name
+  ami                  = data.aws_ami.ami.image_id
   instance_type        = var.OD_INSTANCE_TYPE 
   subnet_id            = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
   vpc_security_group_ids = [aws_security_group.allows_app.id]
