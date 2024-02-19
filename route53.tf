794da722f03688a002fa4f8ae24ecb9bc448f1a0
vinod sjml <vinsjml@gmail.com>
2024-02-19 12:13:39 +0530

    push

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "docdb_dns" {
   zone_id = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID
-  name    = var.INTERNAL ? "${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}" : "${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_NAME}"
+  name    = "${var.COMPONENT}-${var.ENV}"
   type    = "CNAME"
   ttl     = 10
   records = var.INTERNAL ? data.terraform_remote_state.alb_private.PRIVATE_ALB_ADDRESS : data.terraform_remote_state.alb_public.PUBLIC_ALB_ADDRESS
