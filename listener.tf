9ae9c9b27b5f5c914f906cf933921b3c7ad80aa6
vinod sjml <vinsjml@gmail.com>
2024-02-19 17:02:37 +0530

    push

--- listener.tf
+++ listener.tf
@@ -29,14 +29,14 @@ resource "aws_lb_listener_rule" "private_lb_listner_rule" {
   }
 }
 
-resource "aws_lb_listener" "public" {
-   count           = var.INTERNAL ? 0 : 1
-  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
-  port              = "80"
-  protocol          = "HTTPS"
+# resource "aws_lb_listener" "public" {
+#    count           = var.INTERNAL ? 0 : 1
+#   load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+#   port              = "80"
+#   protocol          = "HTTPS"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.app.arn
+#   }
+# }
