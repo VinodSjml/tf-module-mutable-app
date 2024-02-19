a585199567e5cbb9e1d2252bf58431e528c26566
vinod sjml <vinsjml@gmail.com>
2024-02-19 13:29:05 +0530

    push

--- locals.tf
+++ locals.tf
@@ -2,6 +2,6 @@ locals {
   INSTANCE_COUNT = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT
   INSTANCE_IDS   = concat(aws_instance.od.*.id, aws_spot_instance_request.spot.*.spot_instance_id)
   INSTANCE_PRIVATE_IPS = concat(aws_instance.od.*.private_ip, aws_spot_instance_request.spot.*.private_ip)
-  SSH_USER       = centos
-  SSH_PASSWORD   = DevOps321
+  SSH_USER       = "centos"
+  SSH_PASSWORD   = "DevOps321"
 }
