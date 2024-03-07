1cb9f02d68962acad55dbe0eba0975479a7ffbb8
vinod sjml <vinsjml@gmail.com>
2024-03-07 18:06:27 +0530

    push

--- listener.tf
+++ listener.tf
@@ -8,7 +8,7 @@ resource "random_integer" "priority" {
 resource "aws_lb_listener_rule" "private_lb_listner_rule" {
     count      = var.INTERNAL ? 1 : 0
 
-  listener_arn = aws_lb_listener.private.*.arn[0]
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARNLISTENER_ARN
   priority     = random_integer.priority.result
 
   action {
